2ecc86a3aa657ed9e3d1c58d985cdb4315e189c2
Mitchell <m.brown@student.adelaide.edu.au>
2026-08-27 13:22:28 +0930

    Starting Project

---
+++ src/main/java/com/example/demo/Comp3011Assignment1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp3011Assignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comp3011Assignment1Application.class, args);
+		System.out.println("Testing run");
+	}
+
+}
